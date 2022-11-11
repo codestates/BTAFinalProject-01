cab32005da18e0a1b6beb576e5cfbac288e64ae5
Woongsup Jung <woongsupjung@Woongsups-MacBook-Pro.local>
2022-11-12 03:41:27 +0900

    Use different package to generate mnemonic code

--- Wallet/src/APIs/walletAPI.jsx
+++ Wallet/src/APIs/walletAPI.jsx
@@ -1,28 +1,21 @@
-import Neon, { wallet } from "@cityofzion/neon-js";
-// import Seed from "mnemonic-seed-js";
+import { wallet } from "@cityofzion/neon-js";
 const ethers = require('ethers');
-const url = process.env.REACT_APP_PRIVATE_RPC_URL;
-// const rpcClient = Neon.create.rpcClient(url);
-
-// import Account from "../components/Account";
-// var bip39 = require('bip39')
 
 export const createWallet = async (password) => {
-    
-    // const mnemonicCode = wallet.mnemonic.phrase;
-    const privateKey = wallet.generatePrivateKey();
+
+    const mnemonicCode = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32));
+    const mnemonicWallet = ethers.utils.HDNode.fromMnemonic(mnemonicCode, password);
+    const privateKey = mnemonicWallet.privateKey.substring(2);
     const WIF = new wallet.Account(privateKey).WIF;
     const nep2Key = await wallet.encrypt(WIF, password);
-    const userWallet = Neon.create.wallet();
     const userAccount = new wallet.Account(privateKey);
-    userWallet.addAccount(userAccount);
+    
+    // const userWallet = Neon.create.wallet();
+    // const userAccount = new wallet.Account(privateKey);
+    // userWallet.addAccount(userAccount);
+    
     return {'nep2Key': nep2Key,
             'address': userAccount.label,
             'privateKey': privateKey
             };
-    // return new Promise ((result) => 
-    // {result({'nep2Key': nep2Key,
-    // 'address': userAccount.label,
-    // 'mnemonic': mnemonicCode})}
-    // );
 }
