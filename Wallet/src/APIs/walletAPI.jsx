bf495cb69fa38ed000c20de96e382dc9fffc1aaa
Woongsup Jung <woongsupjung@Woongsups-MacBook-Pro.local>
2022-11-12 11:52:14 +0900

    Remove dupilicate creation of account

--- Wallet/src/APIs/walletAPI.jsx
+++ Wallet/src/APIs/walletAPI.jsx
@@ -1,21 +1,32 @@
-import { wallet } from "@cityofzion/neon-js";
+import Neon, { wallet } from "@cityofzion/neon-js";
 const ethers = require('ethers');
+
+const url = process.env.REACT_APP_PRIVATE_RPC_URL;
+const rpcClient = Neon.create.rpcClient(url);
+
+console.log(url);
 
 export const createWallet = async (password) => {
 
     const mnemonicCode = ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32));
     const mnemonicWallet = ethers.utils.HDNode.fromMnemonic(mnemonicCode, password);
     const privateKey = mnemonicWallet.privateKey.substring(2);
-    const WIF = new wallet.Account(privateKey).WIF;
-    const nep2Key = await wallet.encrypt(WIF, password);
     const userAccount = new wallet.Account(privateKey);
-    
+    const nep2Key = await wallet.encrypt(userAccount.WIF, password);
+    console.log(userAccount);
     // const userWallet = Neon.create.wallet();
     // const userAccount = new wallet.Account(privateKey);
     // userWallet.addAccount(userAccount);
 
     return {'nep2Key': nep2Key,
             'address': userAccount.label,
-            'mnemonic': mnemonicCode
+            'mnemonic': mnemonicCode,
+            'publicKey': userAccount.publicKey
             };
 }
+
+// export const checkBalance = async (nep2Key) => {
+//     const res = await rpcClient.getNep17Balances(nep2Key);
+//     console.log(2, res);
+//     return res;
+// }
